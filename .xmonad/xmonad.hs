8437a9e3ce775b1701e58063940b4f55908123c0
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2013-05-19 22:37:12 +0200

    Update xmonad.hs

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,33 +1,55 @@
-import Control.Monad (liftM2)
-import Data.List
+import System.IO
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.EwmhDesktops
 import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers (isFullscreen, isDialog,  doFullFloat, doCenterFloat)
-import qualified XMonad.StackSet as W
+import XMonad.Hooks.ManageHelpers
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
-import System.IO
+import qualified XMonad.StackSet as W
+import Control.Monad (liftM2)
+import Data.List
 
+
+------------------------------------------------------------------------
+-- Terminal
+-- The preferred terminal program, which is used in a binding below and by
+-- certain contrib modules.
+--
 myTerminal = "terminator"
+
 myModMask = mod4Mask
 myNormalBorderColor = "#bada55"
 myFocusedBorderColor = "#ed1c25"
-
 myBorderWidth = 1
 
-myWorkspaces = ["web","chat","dev","media","browse"] ++ map show [6..9]
+------------------------------------------------------------------------
+-- Workspaces
+-- The default number of workspaces (virtual screens) and their names.
+--
+myWorkspaces = ["1:term","2:web","3:code","4:chat","5:media"] ++ map show [6..9]
 
-myManageHook = composeAll . concat $
-    [
-        [className =? c   --> doShift "web" |    c <- myWebApps]
-    ,   [className =? c   --> doShift "chat" |    c <- myChatApps]
-    ,   [isFullscreen --> (doF W.focusDown <+> doFullFloat)]
+------------------------------------------------------------------------
+-- Window rules
+-- Execute arbitrary actions and WindowSet manipulations when managing
+-- a new window. You can use this to, for example, always float a
+-- particular program, or have a client always appear on a particular
+-- workspace.
+--
+-- To find the property name associated with a program, use
+-- > xprop | grep WM_CLASS
+-- and click on the client you're interested in.
+--
+-- To match on the WM_NAME, you can use 'title' in the same way that
+-- 'className' and 'resource' are used below.
+--
+myManageHook = composeAll
+    [ className =? "Chromium"      	--> doShift "2:web"
+    , resource  =? "desktop_window" --> doIgnore
+    , className =? "Steam"          --> doFloat
+    , className =? "Gimp"           --> doFloat
+    , isFullscreen --> (doF W.focusDown <+> doFullFloat)
     ]
-    where
-        myWebApps = ["Chromium"]
-        myChatApps = ["Skype", "Pidgin"]
 
 main = do
     xmproc <- spawnPipe "xmobar"
