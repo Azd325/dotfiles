2b968626c947b5101ce6676a5654d306b9c9ac71
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2013-06-25 15:45:54 +0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,15 +11,36 @@ set __fish_git_prompt_char_stashstate '↩'
 set __fish_git_prompt_char_upstream_ahead '↑'
 set __fish_git_prompt_char_upstream_behind '↓'
 
+set normal (set_color normal)
+set magenta (set_color magenta)
+set yellow (set_color yellow)
+set green (set_color green)
+set gray (set_color -o black)
+
 
 function fish_prompt
-  set last_status $status
+   set_color yellow
+   printf '%s' (whoami)
+   set_color normal
+   printf ' at '
 
-  set_color $fish_color_cwd
-  printf '%s' (prompt_pwd)
-  set_color normal
+   set_color magenta
+   printf '%s' (hostname|cut -d . -f 1)
+   set_color normal
+   printf ' in '
 
-  printf '%s ' (__fish_git_prompt)
+   set_color $fish_color_cwd
+   printf '%s' (prompt_pwd)
+   set_color normal
 
-  set_color normal
+   printf '%s ' (__fish_git_prompt)
+   set_color normal
+
+   # Line 2
+   echo
+   if test $VIRTUAL_ENV
+     printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
+   end
+   printf '↪ '
+   set_color normal
 end
