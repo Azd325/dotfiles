9a75d40e22dc45d1c739896040e7cef8a68440f1
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2014-09-30 15:17:14 +0200

    Updated fish config

---
+++ .config/fish/custom/themes/custom/fish_prompt.fish
@@ -0,0 +1,47 @@
+set fish_greeting ""
+
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '⚡'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '↑'
+set __fish_git_prompt_char_upstream_behind '↓'
+
+set normal (set_color normal)
+set magenta (set_color magenta)
+set yellow (set_color yellow)
+set green (set_color green)
+set gray (set_color -o black)
+
+function fish_prompt
+   set_color yellow
+   printf '%s' (whoami)
+   set_color normal
+   printf ' at '
+
+   set_color magenta
+   printf '%s' (hostname|cut -d . -f 1)
+   set_color normal
+   printf ' in '
+
+   set_color $fish_color_cwd
+   printf '%s' (prompt_pwd)
+   set_color normal
+
+   printf '%s ' (__fish_git_prompt)
+   set_color normal
+
+   if set -q VIRTUAL_ENV
+       echo -n -s (set_color cyan) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
+   end
+end
+
+function myfunc --on-event virtualenv_did_activate
+    echo "The virtualenv '"(basename $VIRTUAL_ENV)"' was activated"
+end
