b990b33f2c2043101fa178bb97fb685662661ca6
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2014-09-30 15:17:14 +0200

    Updated fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,83 +1,22 @@
-set fish_greeting ""
+# Path to your oh-my-fish.
+set fish_path $HOME/.oh-my-fish
 
-# Fish git prompt
-set __fish_git_prompt_showdirtystate 'yes'
-set __fish_git_prompt_showstashstate 'yes'
-set __fish_git_prompt_showupstream 'yes'
-set __fish_git_prompt_color_branch yellow
+# Theme
+set fish_theme custom
 
-# Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '↑'
-set __fish_git_prompt_char_upstream_behind '↓'
+# Which plugins would you like to load? (plugins can be found in ~/.oh-my-fish/plugins/*)
+# Custom plugins may be added to ~/.oh-my-fish/custom/plugins/
+# Example format: set fish_plugins autojump bundler
 
-set normal (set_color normal)
-set magenta (set_color magenta)
-set yellow (set_color yellow)
-set green (set_color green)
-set gray (set_color -o black)
+set fish_plugins archlinux django extract python pyenv tmux vundler
 
-set -x PATH ~/bin $PATH
-set -x PATH ~/.cabal/bin $PATH
-set -x PATH ~/.gem/ruby/2.0.0/bin/ $PATH
-set -x PATH ~/.gem/ruby/2.1.0/bin/ $PATH
-set -x PATH /usr/bin/core_perl/ $PATH
-set -x PATH /opt/android-sdk/platform-tools/ $PATH
-# For normal compiling https://wiki.archlinux.org/index.php/Ccache
-set -x PATH /usr/lib/colorgcc/bin/ $PATH
-set -x PATH /usr/bin/vendor_perl/ $PATH
+# Path to your custom folder (default path is $FISH/custom)
+set fish_custom $HOME/.config/fish/custom
 
-set -x GOPATH "$HOME/tim/go"
+# Load oh-my-fish configuration.
+. $fish_path/oh-my-fish.fish
 
-set -x CCACHE_PATH /usr/bin
-
-set -x EDITOR "vim"
-set -x BROWSER "google-chrome-stable"
-set -x _JAVA_AWT_WM_NONREPARENTING 1
-set -x JAVA_HOME /usr/lib/jvm/java-default-runtime
-
-# Activate Virtualfish
-# https://github.com/adambrenecki/virtualfish
-if test -f ~/.config/fish/virtual.fish
-    . ~/.config/fish/virtual.fish
-    . ~/.config/fish/auto_activation.fish
-    . ~/.config/fish/global_requirements.fish
-    . ~/.config/fish/projects.fish
-    set -x VIRTUALFISH_HOME ~/.virtualenvs
-    set -x VIRTUALFISH_COMPAT_ALIASES 1
-end
-
-# Pip settings
-set -x PIP_LOG_FILE ~/.cache/pip-log.txt
-
-
-function fish_prompt
-   set_color yellow
-   printf '%s' (whoami)
-   set_color normal
-   printf ' at '
-
-   set_color magenta
-   printf '%s' (hostname|cut -d . -f 1)
-   set_color normal
-   printf ' in '
-
-   set_color $fish_color_cwd
-   printf '%s' (prompt_pwd)
-   set_color normal
-
-   printf '%s ' (__fish_git_prompt)
-   set_color normal
-
-   if set -q VIRTUAL_ENV
-       echo -n -s (set_color cyan) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
-   end
-end
-
-function myfunc --on-event virtualenv_did_activate
-    echo "The virtualenv" (basename $VIRTUAL_ENV) "was activated"
-end
-
-test -s /home/tim/.nvm-fish/nvm.fish; and source /home/tim/.nvm-fish/nvm.fish
+. $fish_custom/plugins/virtualfish/virtual.fish
+. $fish_custom/plugins/virtualfish/auto_activation.fish
+. $fish_custom/plugins/virtualfish/global_requirements.fish
+. $fish_custom/plugins/virtualfish/projects.fish
