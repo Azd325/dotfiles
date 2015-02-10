e0ebcdeffe26b6cd9ccb4c3548b7a8806fb9bc09
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2015-02-10 23:13:22 +0100

    Added git prompt to fish

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,4 +1,28 @@
+set -g __fish_git_prompt_show_informative_status 1
+set -g __fish_git_prompt_hide_untrackedfiles 1
+
+set -g __fish_git_prompt_color_branch magenta bold
+set -g __fish_git_prompt_showupstream "informative"
+set -g __fish_git_prompt_char_upstream_ahead "↑"
+set -g __fish_git_prompt_char_upstream_behind "↓"
+set -g __fish_git_prompt_char_upstream_prefix ""
+
+set -g __fish_git_prompt_char_stagedstate "●"
+set -g __fish_git_prompt_char_dirtystate "✚"
+set -g __fish_git_prompt_char_untrackedfiles "…"
+set -g __fish_git_prompt_char_conflictedstate "✖"
+set -g __fish_git_prompt_char_cleanstate "✔"
+
+set -g __fish_git_prompt_color_dirtystate blue
+set -g __fish_git_prompt_color_stagedstate yellow
+set -g __fish_git_prompt_color_invalidstate red
+set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
+set -g __fish_git_prompt_color_cleanstate green bold
+
+
 function fish_prompt
+    set -l last_status $status
+
 	set_color yellow
     printf '%s' (whoami)
     set_color normal
@@ -12,6 +36,12 @@ function fish_prompt
     set_color $fish_color_cwd
     printf '%s' (prompt_pwd)
     set_color normal
+
+    printf '%s ' (__fish_git_prompt)
+
+    if not test $last_status -eq 0
+        set_color $fish_color_error
+    end
 
     if test $VIRTUAL_ENV
         printf " (%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
