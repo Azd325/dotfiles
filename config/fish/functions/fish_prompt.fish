c931d60d2f5e1ddd196decaef8b06f40b6f61d46
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2015-07-16 18:07:38 +0200

    Update fish prompt

--- config/fish/functions/fish_prompt.fish
+++ config/fish/functions/fish_prompt.fish
@@ -1,54 +1,22 @@
-set -g __fish_git_prompt_show_informative_status 1
-set -g __fish_git_prompt_hide_untrackedfiles 1
+function fish_prompt --description 'Write out the prompt'
+	set -l last_status $status
 
-set -g __fish_git_prompt_color_branch magenta bold
-set -g __fish_git_prompt_showupstream "informative"
-set -g __fish_git_prompt_char_upstream_ahead "↑"
-set -g __fish_git_prompt_char_upstream_behind "↓"
-set -g __fish_git_prompt_char_upstream_prefix ""
+	if not set -q __fish_prompt_normal
+		set -g __fish_prompt_normal (set_color normal)
+	end
 
-set -g __fish_git_prompt_char_stagedstate "●"
-set -g __fish_git_prompt_char_dirtystate "✚"
-set -g __fish_git_prompt_char_untrackedfiles "…"
-set -g __fish_git_prompt_char_conflictedstate "✖"
-set -g __fish_git_prompt_char_cleanstate "✔"
+	# PWD
+	set_color $fish_color_cwd
+	echo -n (prompt_pwd)
+	set_color normal
 
-set -g __fish_git_prompt_color_dirtystate blue
-set -g __fish_git_prompt_color_stagedstate yellow
-set -g __fish_git_prompt_color_invalidstate red
-set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
-set -g __fish_git_prompt_color_cleanstate green bold
+	printf '%s ' (__fish_git_prompt)
 
+	if not test $last_status -eq 0
+	set_color $fish_color_error
+	end
 
-function fish_prompt
-    set -l last_status $status
+	echo -n '$ '
 
-	set_color yellow
-    printf '%s' (whoami)
-    set_color normal
-    printf ' at '
-
-    set_color magenta
-    printf '%s' (hostname|cut -d . -f 1)
-    set_color normal
-    printf ' in '
-
-    set_color $fish_color_cwd
-    printf '%s' (prompt_pwd)
-    set_color normal
-
-    printf '%s ' (__fish_git_prompt)
-
-    if not test $last_status -eq 0
-        set_color $fish_color_error
-    end
-
-    if test $VIRTUAL_ENV
-        printf " (%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
-    end
-
-    echo
-
-    printf '↪ '
-    set_color normal
+	set_color normal
 end
