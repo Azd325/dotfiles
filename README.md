35c96a79b5cd05030183348b01567c07911672fb
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2017-08-23 17:18:45 +0200

    Remove legacy stuff

--- README.md
+++
@@ -1,7 +0,0 @@
-# dotfiles
-My dotfiles are using https://github.com/anishathalye/dotbot libary to orgnaize and install them everywhere.
-
-
-## Install
-
-    bash install
--- screenlayout/hdmi.screen.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI1 --mode 1920x1080 --pos 0x0 --rotate normal --output LVDS1 --mode 1366x768 --pos 1920x0 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output VGA1 --off
