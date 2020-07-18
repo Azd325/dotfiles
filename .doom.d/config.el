9a3e6273b8f4b504ed1b91061202743eb0b1376d
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2020-07-18 12:20:11 +0200

    Add initital doom config

---
+++ .doom.d/config.el
@@ -0,0 +1,40 @@
+(setq user-full-name "Tim kleinschmidt"
+      user-mail-address "tim.kleinschmidt@gmail.com")
+
+(setq doom-theme 'doom-nord)
+
+(setq doom-font (font-spec :family "JetBrains Mono" :size 12 :line-spacing 1.2)
+      doom-variable-pitch-font (font-spec :family "JetBrains Mono")
+      doom-unicode-font (font-spec :family "DejaVu Sans")
+      doom-big-font (font-spec :family "JetBrains Mono" :size 20))
+
+(add-to-list 'ivy-re-builders-alist '(counsel-projectile-find-file . ivy--regex-plus))
+
+(setq display-line-numbers-type nil)
+
+(setq ns-right-alternate-modifier 'none)
+
+(setq company-idle-delay nil)
+(add-hook 'window-setup-hook #'toggle-frame-maximized)
+(add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
+
+(setq org-directory "~/Google Drive/Documents/emacs/org/"
+      org-archive-location (concat org-directory ".archive/%s::")
+      org-roam-directory (concat org-directory "notes/")
+      org-journal-encrypt-journal t)
+
+(setq magit-repository-directories '(("~/" . 2))
+      magit-save-repository-buffers nil
+      transient-values '((magit-commit "--gpg-sign=3F74D3A286A02EED")
+                         (magit-rebase "--autosquash" "--gpg-sign=3F74D3A286A02EED")
+                         (magit-pull "--rebase" "--gpg-sign=3F74D3A286A02EED")))
+
+(setq cider-boot-parameters "dev --cider"
+      clojure-align-forms-automatically t)
+
+(defun clojurescript-mode-before-save-hook ()
+  (interactive)
+  (when (eq major-mode 'clojurescript-mode)
+    (indent-region (point-min) (point-max))))
+
+(add-hook 'before-save-hook #'clojurescript-mode-before-save-hook)
---
+++ .doom.d/init.el
@@ -0,0 +1,116 @@
+;;; init.el -*- lexical-binding: t; -*-
+
+;; Copy this file to ~/.doom.d/init.el or ~/.config/doom/init.el ('doom install'
+;; will do this for you). The `doom!' block below controls what modules are
+;; enabled and in what order they will be loaded. Remember to run 'doom refresh'
+;; after modifying it.
+;;
+;; More information about these modules (and what flags they support) can be
+;; found in modules/README.org.
+
+(doom! :completion
+       (company +auto
+                +childframe)
+       (ivy +fuzzy
+            +prescient
+            +icons)
+
+       :ui
+       deft
+       doom              ; what makes DOOM look the way it does
+       doom-dashboard    ; a nifty splash screen for Emacs
+       fill-column       ; a `fill-column' indicator
+       hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
+       indent-guides     ; highlighted indent columns
+       modeline          ; snazzy, Atom-inspired modeline, plus API
+       minimap
+       nav-flash         ; blink the current line after jumping
+       ophints           ; highlight the region an operation acts on
+       (popup            ; tame sudden yet inevitable temporary windows
+        +defaults)       ; default popup rules
+       treemacs          ; a project drawer, like neotree but cooler
+       tabs
+       vc-gutter         ; vcs diff in the fringe
+       vi-tilde-fringe   ; fringe tildes to mark beyond EOB
+       window-select     ; visually switch windows
+       workspaces        ; tab emulation, persistence & separate workspaces
+       zen
+
+       :editor
+       (evil +everywhere); come to the dark side, we have cookies
+       file-templates    ; auto-snippets for empty files
+       fold              ; (nigh) universal code folding
+       (format +onsave)  ; automated prettiness
+       multiple-cursors  ; editing in many places at once
+       ;;parinfer          ; turn lisp into python, sort of
+       rotate-text       ; cycle region at point between text candidates
+       snippets          ; my elves. They type so I don't have to
+
+       :emacs
+       (dired +ranger
+              +icons)             ; making dired pretty [functional]
+       electric          ; smarter, keyword-based electric-indent
+       (ibuffer +icons)           ; interactive buffer management
+       undo
+       vc                ; version-control and Emacs, sitting in a tree
+
+       :term
+       vterm
+
+       :checkers
+       syntax              ; tasing you for every semicolon you forget
+       spell             ; tasing you for misspelling mispelling
+       grammar           ; tasing grammar mistake every you make
+
+       :tools
+       ansible
+       ;;docker
+       editorconfig      ; let someone else argue about tabs vs spaces
+       (eval +overlay)     ; run code, run (also, repls)
+       ;;gist              ; interacting with github gists
+       (lookup           ; helps you navigate your code and documentation
+        +dictionary
+        +docsets)        ; ...or in Dash docsets locally
+       lsp
+       macos             ; MacOS-specific commands
+       (magit +forge)             ; a git porcelain for Emacs
+       ;;make              ; run make tasks from Emacs
+       ;;pdf               ; pdf enhancements
+       rgb               ; creating color strings
+       ;;tmux              ; an API for interacting with tmux
+       taskrunner
+                                        ;,upload            ; map local to remote projects via ssh/ftp
+
+       :lang
+       (clojure)           ; java with a lisp
+       common-lisp       ; if you've seen one lisp, you've seen them all
+       data              ; config/data formats
+       emacs-lisp        ; drown in parentheses
+       (javascript +lsp)
+       latex             ; writing papers in Emacs has never been so fun
+       markdown          ; writing docs for people to ignore
+       nix
+       yaml
+       (org
+        +dragndrop
+        +gnuplot
+        +journal
+        +jupyter
+        +pandoc
+        +pomodoro
+        +present
+        +roam)
+       (php               ; perl's insecure younger brother
+        +lsp)
+       (python            ; beautiful is better than ugly
+        +pyenv
+        +lsp)
+       ruby
+       ;;rest              ; Emacs as a REST client
+       ;;rst
+       (sh +lsp)                ; she sells {ba,z,fi}sh shells on the C xor
+       web               ; the tubes
+
+       :config
+
+       (default +bindings +smartparens))
