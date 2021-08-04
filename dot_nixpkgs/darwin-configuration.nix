e8c52c921194fe0c17a596923d8d267374f1a85c
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2021-08-04 09:30:44 +0200

    Sort packages

--- dot_nixpkgs/darwin-configuration.nix
+++ dot_nixpkgs/darwin-configuration.nix
@@ -16,6 +16,7 @@
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
+    #yarn
     boot
     chezmoi
     coreutils
@@ -43,7 +44,6 @@
     tmux
     vagrant
     vim
-    #yarn
     youtube-dl
     zsh
   ];
