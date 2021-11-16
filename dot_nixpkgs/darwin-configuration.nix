cf106d570e67a7197c43674a4163847c7e6e916c
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2021-11-16 14:47:11 +0100

    Disable completion
    https://github.com/NixOS/nix/issues/5445

--- dot_nixpkgs/darwin-configuration.nix
+++ dot_nixpkgs/darwin-configuration.nix
@@ -117,7 +117,7 @@
   # nix.package = pkgs.nix;
 
   # Create /etc/bashrc that loads the nix-darwin environment.
-  programs.zsh.enable = true; # default shell on catalina
+  programs.zsh.enable = false; # default shell on catalina
   # programs.fish.enable = true;
 
   # Used for backwards compatibility, please read the changelog before changing.
