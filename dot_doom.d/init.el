b4b22873b3c2a947f3cb96c9657c860349bfdcaa
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2021-11-23 10:12:49 +0100

    Enable web module

--- dot_doom.d/init.el
+++ dot_doom.d/init.el
@@ -167,7 +167,7 @@
        ;;solidity          ; do you need a blockchain? No.
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
-       ;;web               ; the tubes
+       web               ; the tubes
        ;;yaml              ; JSON, but readable
        ;;zig               ; C, but simpler
 
