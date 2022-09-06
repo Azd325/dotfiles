ae80d8b6976835a801df195835fc0b7c8ee25fe4
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2022-09-06 11:00:41 +0200

    Bump nixpkgs

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
 
   inputs = {
     # Package sets
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-22.05-darwin";
-    nixpkgs-unstable.url = github:NixOS/nixpkgs/nixpkgs-unstable;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-22.05-darwin";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     # Environment/system management
     darwin.url = "github:lnl7/nix-darwin/master";
@@ -13,62 +13,64 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
     # Other sources
-    comma = { url = github:Shopify/comma; flake = false; };
-    
+    comma = {
+      url = "github:Shopify/comma";
+      flake = false;
+    };
+
   };
 
   outputs = { self, darwin, nixpkgs, home-manager, ... }@inputs:
-  let 
+    let
 
-    inherit (darwin.lib) darwinSystem;
-    inherit (inputs.nixpkgs-unstable.lib) attrValues makeOverridable optionalAttrs singleton;
+      inherit (darwin.lib) darwinSystem;
+      inherit (inputs.nixpkgs-unstable.lib)
+        attrValues makeOverridable optionalAttrs singleton;
 
-    # Configuration for `nixpkgs`
-    nixpkgsConfig = {
-      config = { allowUnfree = true; };
-    };
-  in
-  {
-    # My `nix-darwin` configs
-      
-    darwinConfigurations = rec {
-      BER = darwinSystem {
-        system = "aarch64-darwin";
-        modules = [
-          # Main `nix-darwin` config
-          ./darwin-configuration.nix
-          # `home-manager` module
-          home-manager.darwinModules.home-manager
-          {
-            nixpkgs = nixpkgsConfig;
-            # `home-manager` config
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.timkleinschmidt = import ./home.nix;
-          }
-        ];
-      };
-    };
+      # Configuration for `nixpkgs`
+      nixpkgsConfig = { config = { allowUnfree = true; }; };
+    in {
+      # My `nix-darwin` configs
 
-    # Overlays --------------------------------------------------------------- {{{
-
-    overlays = {
-      # Overlays to add various packages into package set
-        comma = final: prev: {
-          comma = import inputs.comma { inherit (prev) pkgs; };
-        };  
-
-      # Overlay useful on Macs with Apple Silicon
-        apple-silicon = final: prev: optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
-          # Add access to x86 packages system is running Apple Silicon
-          pkgs-x86 = import inputs.nixpkgs-unstable {
-            system = "x86_64-darwin";
-            inherit (nixpkgsConfig) config;
-          };
+      darwinConfigurations = rec {
+        BER = darwinSystem {
+          system = "aarch64-darwin";
+          modules = [
+            # Main `nix-darwin` config
+            ./darwin-configuration.nix
+            # `home-manager` module
+            home-manager.darwinModules.home-manager
+            {
+              nixpkgs = nixpkgsConfig;
+              # `home-manager` config
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.timkleinschmidt = import ./home.nix;
+            }
+          ];
         };
       };
 
-    # My `nix-darwin` modules that are pending upstream, or patched versions waiting on upstream
-    # fixes.
- };
+      # Overlays --------------------------------------------------------------- {{{
+
+      overlays = {
+        # Overlays to add various packages into package set
+        comma = final: prev: {
+          comma = import inputs.comma { inherit (prev) pkgs; };
+        };
+
+        # Overlay useful on Macs with Apple Silicon
+        apple-silicon = final: prev:
+          optionalAttrs (prev.stdenv.system == "aarch64-darwin") {
+            # Add access to x86 packages system is running Apple Silicon
+            pkgs-x86 = import inputs.nixpkgs-unstable {
+              system = "x86_64-darwin";
+              inherit (nixpkgsConfig) config;
+            };
+          };
+      };
+
+      # My `nix-darwin` modules that are pending upstream, or patched versions waiting on upstream
+      # fixes.
+    };
 }
