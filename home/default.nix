c4ccebab4b5211b188ad3d52968f3b55c714a317
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2023-03-29 06:55:56 +0200

    Update home-manager to 23.05

--- home/default.nix
+++ home/default.nix
@@ -1,7 +1,7 @@
 { ... }: {
   imports = [ ./files ./packages.nix ./programs ];
 
-  home.stateVersion = "22.11";
+  home.stateVersion = "23.05";
 
   home.username = "timkleinschmidt";
   home.homeDirectory = "/Users/timkleinschmidt";
