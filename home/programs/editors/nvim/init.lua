eb5b3cca4edf3803282659e23a2a5802aa918163
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2023-03-31 21:11:32 +0200

    Add vim config

---
+++ home/programs/editors/nvim/init.lua
@@ -0,0 +1,85 @@
+-- sets
+
+vim.opt.relativenumber = true
+
+-- remaps
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+vim.keymap.set("n", "<leader><leader>", function()
+  vim.cmd("so")
+end)
+
+-- telescope
+
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set("n", "<leader>ps", function()
+  builtin.grep_string({ search = vim.fn.input("Grep > ") })
+end)
+vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
+
+-- rose-pine
+
+vim.cmd("colorscheme rose-pine")
+require("rose-pine").setup({
+  disable_background = true,
+})
+
+function ColorMyPencils(color)
+  color = color or "rose-pine"
+  vim.cmd.colorscheme(color)
+
+  vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+end
+
+ColorMyPencils()
+
+-- trouble
+vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
+
+-- treesitter
+
+require("nvim-treesitter.configs").setup({
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+})
+
+-- harpoon
+
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<C-h>", function()
+  ui.nav_file(1)
+end)
+vim.keymap.set("n", "<C-t>", function()
+  ui.nav_file(2)
+end)
+vim.keymap.set("n", "<C-n>", function()
+  ui.nav_file(3)
+end)
+vim.keymap.set("n", "<C-s>", function()
+  ui.nav_file(4)
+end)
+
+-- lsp
+local lsp = require("lsp-zero")
+
+lsp.preset("recommended")
