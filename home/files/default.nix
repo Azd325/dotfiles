53bf56f0753928d8abc6b300cefdbb4ddd8e8069
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2023-04-01 21:21:19 +0200

    Add rose-pine-warp theme
    https://github.com/thanhsonng/rose-pine-warp

--- home/files/default.nix
+++ home/files/default.nix
@@ -1,6 +1,7 @@
-{ ... }: {
+{ rose-pine-warp, ... }: {
   home.file.".config/doom" = { source = ./doom-emacs; };
   home.file.".shadow-cljs" = { source = ./shadow-cljs; };
   home.file.".yarnrc.yml" = { source = ./.yarnrc.yml; };
   home.file.".zprofile" = { source = ./.zprofile; };
+  home.file.".warp/themes/rose-pine-warp" = { source = rose-pine-warp; };
 }
