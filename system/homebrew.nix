84142ea7b69605d82da0cc84aa0257b548248879
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2023-04-15 14:37:07 +0200

    fix(brew): Disable Testflight
    https://github.com/mas-cli/mas/issues/483

--- system/homebrew.nix
+++ system/homebrew.nix
@@ -22,7 +22,7 @@
       Keynote = 409183694;
       Numbers = 409203825;
       Pages = 409201541;
-      TestFlight = 899247664;
+      #TestFlight = 899247664;
       WireGuard = 1451685025;
       Xcode = 497799835;
     };
