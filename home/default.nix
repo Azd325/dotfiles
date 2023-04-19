5fa2735a0ed4275c420404b6e382316461285f2d
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2023-04-19 14:30:12 +0200

    chore: Move sessionvariables definition

--- home/default.nix
+++ home/default.nix
@@ -8,12 +8,14 @@
   manual.manpages.enable = false;
   programs.man.enable = false;
 
-  home.sessionVariables = {
-    PATH =
-      "/Users/timkleinschmidt/.config/emacs/bin:/Applications/Postgres.app/Contents/Versions/latest/bin:\${PATH}";
-    LC_ALL = "en_US.UTF-8";
-    LANG = "en_US.UTF-8";
-    GDAL_LIBRARY_PATH = "/opt/homebrew/opt/gdal/lib/libgdal.dylib";
-    GEOS_LIBRARY_PATH = "/opt/homebrew/opt/geos/lib/libgeos_c.dylib";
+  home = {
+    sessionVariables = {
+      PATH =
+        "/Users/timkleinschmidt/.config/emacs/bin:/Applications/Postgres.app/Contents/Versions/latest/bin:\${PATH}";
+      LC_ALL = "en_US.UTF-8";
+      LANG = "en_US.UTF-8";
+      GDAL_LIBRARY_PATH = "/opt/homebrew/opt/gdal/lib/libgdal.dylib";
+      GEOS_LIBRARY_PATH = "/opt/homebrew/opt/geos/lib/libgeos_c.dylib";
+    };
   };
 }
