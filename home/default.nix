5d58775e1f16d2962451eb08169ea26566be118a
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2023-04-19 14:32:31 +0200

    refactor: Use sessionPath instead to construct it

--- home/default.nix
+++ home/default.nix
@@ -9,9 +9,11 @@
   programs.man.enable = false;
 
   home = {
+    sessionPath = [
+      "/Users/timkleinschmidt/.config/emacs/bin"
+      "/Applications/Postgres.app/Contents/Versions/latest/bin"
+    ];
     sessionVariables = {
-      PATH =
-        "/Users/timkleinschmidt/.config/emacs/bin:/Applications/Postgres.app/Contents/Versions/latest/bin:\${PATH}";
       LC_ALL = "en_US.UTF-8";
       LANG = "en_US.UTF-8";
       GDAL_LIBRARY_PATH = "/opt/homebrew/opt/gdal/lib/libgdal.dylib";
