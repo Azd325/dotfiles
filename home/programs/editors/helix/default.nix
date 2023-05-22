c81b6af897c377ab7a1de85d3064ebdd06fdaef2
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2023-05-22 10:30:47 +0200

    chore(helix): migrate to new language syntax

--- home/programs/editors/helix/default.nix
+++ home/programs/editors/helix/default.nix
@@ -11,38 +11,40 @@
 
   programs.helix = {
     enable = true;
-    languages = [
-      {
-        name = "lua";
-        auto-format = true;
-        formatter = {
-          command = "${pkgs.stylua.out}/bin/stylua";
-          args = [ "-" ];
-        };
-      }
-      {
-        name = "python";
-        auto-format = true;
-        formatter = {
-          command = "${pkgs.python311Packages.black.out}/bin/black";
-          args = [ "--quiet" "-" ];
-        };
-      }
-      {
-        name = "nix";
-        auto-format = true;
-        formatter = { command = "${pkgs.nixfmt.out}/bin/nixfmt"; };
-      }
-      {
-        name = "clojure";
-        auto-format = true;
-        formatter = {
-          # binary not managed by nix
-          command = "cljfmt";
-          args = [ "fix" "-" ];
-        };
-      }
-    ];
+    languages = {
+      language = [
+        {
+          name = "lua";
+          auto-format = true;
+          formatter = {
+            command = "${pkgs.stylua.out}/bin/stylua";
+            args = [ "-" ];
+          };
+        }
+        {
+          name = "python";
+          auto-format = true;
+          formatter = {
+            command = "${pkgs.python311Packages.black.out}/bin/black";
+            args = [ "--quiet" "-" ];
+          };
+        }
+        {
+          name = "nix";
+          auto-format = true;
+          formatter = { command = "${pkgs.nixfmt.out}/bin/nixfmt"; };
+        }
+        {
+          name = "clojure";
+          auto-format = true;
+          formatter = {
+            # binary not managed by nix
+            command = "cljfmt";
+            args = [ "fix" "-" ];
+          };
+        }
+      ];
+    };
     settings = {
       editor = { line-number = "relative"; };
       theme = "rose_pine";
