ca8a95101a46c8838046129099f612ea4f296003
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2023-05-25 20:18:42 +0200

    feat(vscode): disable user settings

--- home/programs/editors/vscode/default.nix
+++ home/programs/editors/vscode/default.nix
@@ -1,8 +1,6 @@
 { pkgs, ... }: {
   programs.vscode = {
     enable = true;
-    enableExtensionUpdateCheck = false;
-    enableUpdateCheck = false;
     extensions = with pkgs.vscode-extensions; [
       betterthantomorrow.calva
       bungcip.better-toml
@@ -17,17 +15,5 @@
       ms-vscode.makefile-tools
       redhat.vscode-yaml
     ];
-    userSettings = {
-      workbench = {
-        colorTheme = "Dracula";
-        tree = { indent = 20; };
-      };
-      editor = {
-        inlineSuggest = { enabled = true; };
-        fontFamily = "JetBrains Mono";
-        fontSize = 15;
-      };
-      redhat = { telemetry = { enabled = false; }; };
-    };
   };
 }
