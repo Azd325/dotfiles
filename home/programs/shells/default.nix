e485f1de3c4bca8ac29a81eb75f0666cfe08bb51
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2023-06-28 12:42:28 +0200

    fix: rename zsh config variable
    https://github.com/nix-community/home-manager/pull/4122

--- home/programs/shells/default.nix
+++ home/programs/shells/default.nix
@@ -2,7 +2,7 @@
   programs.zsh = {
     enable = true;
     enableAutosuggestions = true;
-    enableSyntaxHighlighting = true;
+    syntaxHighlighting.enable = true;
     initExtra = ''
       # FNM
       eval "$(fnm env --use-on-cd)"
