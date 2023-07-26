70c781faee5577e8401ed7b9873763878e51e48c
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2023-07-26 09:37:12 +0200

    chore(vscode): sync plugins

--- home/programs/editors/vscode/default.nix
+++ home/programs/editors/vscode/default.nix
@@ -6,12 +6,15 @@
       charliermarsh.ruff
       eamodio.gitlens
       editorconfig.editorconfig
+      esbenp.prettier-vscode
       github.copilot
       github.vscode-github-actions
+      github.vscode-pull-request-github
       kahole.magit
       mechatroner.rainbow-csv
       ms-azuretools.vscode-docker
       ms-python.python
+      ms-vscode-remote.remote-containers
       ms-vscode.makefile-tools
       redhat.vscode-yaml
       tamasfe.even-better-toml
