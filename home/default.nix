474a698424168fcd007935a30090e9982faf375a
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2023-11-17 12:13:20 +0100

    feat: add android env variables

--- home/default.nix
+++ home/default.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ config, ... }: {
   imports = [ ./files ./packages.nix ./programs ];
 
   home.stateVersion = "23.11";
@@ -16,12 +16,19 @@
       "/Users/timkleinschmidt/.config/emacs/bin"
       # postgres helper e.g createdb, dropdb, psql
       "/Applications/Postgres.app/Contents/Versions/latest/bin"
+      # Android SDK
+      "$ANDROID_SDK_ROOT/emulator"
+      "$ANDROID_SDK_ROOT/platform-tools"
+      "$ANDROID_SDK_ROOT/tools"
     ];
     sessionVariables = {
       LC_ALL = "en_US.UTF-8";
       LANG = "en_US.UTF-8";
       GDAL_LIBRARY_PATH = "/opt/homebrew/opt/gdal/lib/libgdal.dylib";
       GEOS_LIBRARY_PATH = "/opt/homebrew/opt/geos/lib/libgeos_c.dylib";
+      # Android SDK
+      ANDROID_SDK_ROOT = "$HOME/Library/Android/sdk";
+      ANDROID_SDK = "${config.home.sessionVariables.ANDROID_SDK_ROOT}";
     };
   };
 }
