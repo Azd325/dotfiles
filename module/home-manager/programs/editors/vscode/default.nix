b9bc8b61eed7f3c0e7e080ce8b7f65418bca072d
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2025-02-25 09:40:05 +0100

    refactor(vscode): use profile extensions settings

--- module/home-manager/programs/editors/vscode/default.nix
+++ module/home-manager/programs/editors/vscode/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   programs.vscode = {
     enable = true;
-    extensions = with pkgs.vscode-extensions; [
+    profiles.default.extensions = with pkgs.vscode-extensions; [
       betterthantomorrow.calva
       charliermarsh.ruff
       dracula-theme.theme-dracula
