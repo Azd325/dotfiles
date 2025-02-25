f0977552f696404d4c83f33eff930d958d5e4bd9
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2025-02-25 09:39:45 +0100

    refactor(darwin): rename pam settings

--- system/darwin.nix
+++ system/darwin.nix
@@ -18,7 +18,7 @@ inputs.darwin.lib.darwinSystem {
       networking.hostName = "BER";
 
       # Add ability to used TouchID for sudo authentication
-      security.pam.enableSudoTouchIdAuth = true;
+      security.pam.services.sudo_local.touchIdAuth = true;
 
       system = {
         # Used for backwards compatibility, please read the changelog before changing.
