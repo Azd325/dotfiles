4e8c628df6b525dbf2be887f58282f37e5c69da9
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2025-10-04 19:02:23 +0200

    add numtide binary cache

--- module/configuration.nix
+++ module/configuration.nix
@@ -30,10 +30,12 @@ in
       substituters = [
         "https://nix-community.cachix.org"
         "https://cache.nixos.org"
+        "https://numtide.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
       ];
       trusted-users = trustedUsers;
       warn-dirty = false;
