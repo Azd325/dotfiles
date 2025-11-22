ca198cf0f0bf8b2e8f26c3c6a179fd73118aec62
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2025-11-22 13:21:13 +0100

    feat: add opencode config

--- module/home-manager/files/default.nix
+++ module/home-manager/files/default.nix
@@ -4,6 +4,7 @@ _: {
     ".shadow-cljs".source = ./shadow-cljs;
     ".yarnrc.yml".source = ./.yarnrc.yml;
     ".zprofile".source = ./.zprofile;
+    ".config/opencode/opencode.json".source = ./opencode.json;
     ".gemrc".text = "gem: --no-document";
   };
 }
