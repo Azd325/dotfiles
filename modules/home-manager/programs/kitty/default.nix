7e42f78e2c6b3cd230d2fb3e4ee5f2c274ddd8db
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2025-12-19 12:06:49 +0100

    feat(kitty): add keyboard-layout-agnostic tab navigation shortcuts

--- modules/home-manager/programs/kitty/default.nix
+++ modules/home-manager/programs/kitty/default.nix
@@ -39,10 +39,33 @@ _:
     };
 
     keybindings = {
+      # Tab management
       "cmd+t" = "new_tab";
       "cmd+w" = "close_tab";
+
+      # Tab navigation - keyboard-layout-agnostic shortcuts
+      # Direct tab access (works on all keyboard layouts)
+      "cmd+1" = "goto_tab 1";
+      "cmd+2" = "goto_tab 2";
+      "cmd+3" = "goto_tab 3";
+      "cmd+4" = "goto_tab 4";
+      "cmd+5" = "goto_tab 5";
+      "cmd+6" = "goto_tab 6";
+      "cmd+7" = "goto_tab 7";
+      "cmd+8" = "goto_tab 8";
+      "cmd+9" = "goto_tab 9";
+
+      # Next/previous tab (universal shortcuts that work across layouts)
+      "ctrl+tab" = "next_tab";
+      "ctrl+shift+tab" = "previous_tab";
+      "cmd+shift+right" = "next_tab";
+      "cmd+shift+left" = "previous_tab";
+
+      # Keep US keyboard shortcuts for compatibility
       "cmd+shift+]" = "next_tab";
       "cmd+shift+[" = "previous_tab";
+
+      # Font size
       "cmd+plus" = "change_font_size all +1.0";
       "cmd+minus" = "change_font_size all -1.0";
       "cmd+0" = "change_font_size all 0";
