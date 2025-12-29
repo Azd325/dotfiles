123427ecfdc6ba84d9382a9dc4ec1b1ea3ed8c20
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2025-12-29 22:56:47 +0100

    Tune WezTerm font rendering

--- modules/home-manager/programs/wezterm/default.nix
+++ modules/home-manager/programs/wezterm/default.nix
@@ -2,5 +2,20 @@ _:
 {
   programs.wezterm = {
     enable = true;
+
+    # Comfortable font size for typical DPI setups
+    extraConfig = ''
+      return {
+        font_size = 15.0,
+        line_height = 1.05,
+        cell_width = 0.98,
+        harfbuzz_features = {
+          "liga=0",
+          "clig=0",
+          "calt=0",
+        },
+        front_end = "WebGpu",
+      }
+    '';
   };
 }
