4a9dfbf563293cf5203bd233b6e9913d5846e6b2
Tim Kleinschmidt <tim.kleinschmidt@gmail.com>
2025-12-31 19:39:12 +0100

    fix: allow Option dead keys in wezterm

--- modules/home-manager/programs/wezterm/default.nix
+++ modules/home-manager/programs/wezterm/default.nix
@@ -1,11 +1,12 @@
-_:
-{
+_: {
   programs.wezterm = {
     enable = true;
 
     # Comfortable font size for typical DPI setups
     extraConfig = ''
       return {
+        send_composed_key_when_left_alt_is_pressed = true,
+        send_composed_key_when_right_alt_is_pressed = true,
         font_size = 15.0,
         line_height = 1.05,
         cell_width = 0.98,
